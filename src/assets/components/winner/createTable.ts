14b471dba2b8fc287db36823d339003cad54ced4
kapkande <kap.kande@gmail.com>
2023-01-26 00:12:36 +0300

    init: add arrow sort

--- src/assets/components/winner/createTable.ts
+++ src/assets/components/winner/createTable.ts
@@ -4,6 +4,8 @@ import objWinners from '../date/ObjectWinners'
 
 
 async function createTable() {
+    console.log(await interactionWinners.getWinners('?_order=ASC, ?_sort=id'))
+    console.log(await interactionWinners.getWinners('?_order=DESC ?_sort=id'));
     const winners:Array<objWinners> = await interactionWinners.getWinners();
     winners.forEach(element => {
         const date:objWinners = {
--- src/assets/components/winner/createTableHeader.ts
+++ src/assets/components/winner/createTableHeader.ts
@@ -26,16 +26,28 @@ trHeader.appendChild(thHeaderName);
 let thHeaderWins = document.createElement('th');
 thHeaderWins.classList.add('winners__thWins');
 thHeaderWins.textContent = 'Wins'
+createArrows(thHeaderWins)
 trHeader.appendChild(thHeaderWins);
 
 let thBastTime = document.createElement('th');
 thBastTime.classList.add('winners__thBastTime');
 thBastTime.textContent = 'Bast Time'
+createArrows(thBastTime)
 trHeader.appendChild(thBastTime);
 // th close
 table?.appendChild(trHeader);
 
 
+function createArrows(th:HTMLElement) {
+    let arrow = document.createElement('span')
+    arrow.innerHTML = `
+    <span class="anticon sorter-up">
+    <svg viewBox="0 0 1024 1024" focusable="false" data-icon="caret-up" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M858.9 689L530.5 308.2c-9.4-10.9-27.5-10.9-37 0L165.1 689c-12.2 14.2-1.2 35 18.5 35h656.8c19.7 0 30.7-20.8 18.5-35z"></path></svg></span>
+    <span class="anticon sorter-down">
+    <svg viewBox="0 0 1024 1024" focusable="false" data-icon="caret-down" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"></path></svg></span>
+   `
+   th.appendChild(arrow);
+}
 // `<table>
 //   <tr>
 //     <th>№ п/п</th>
